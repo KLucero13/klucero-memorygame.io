156bb102bb284498e0839a9361df6887924d44b6
KLucero13 <KLucero13@users.noreply.github.com>
2015-03-12 13:19:18 -0400

    Update application.js

--- application.js
+++ application.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
   for(i=1; i<21; i++){
     $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
   }
-});
+};
+card1="none",
+    $('.card').on('click',function(){
+      $mycard=$(this).find('p');
+      $mycard.css('opacity',1);
+      card1=$mycard.html();
+    }
+    
