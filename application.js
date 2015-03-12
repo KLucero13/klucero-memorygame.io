35b54855f3bd735715a6241936976c8043cc6736
KLucero13 <KLucero13@users.noreply.github.com>
2015-03-12 13:26:58 -0400

    Update application.js

--- application.js
+++ application.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  var card_array=["fish","fish","turtle","turtle","pig","pig","dog","dog","cat","cat","snake","snake","bird","bird","goat","goat","hampster","hampster","dragon","dragon"]
+}
+$(document).ready(function(){
   for(i=1; i<21; i++){
     $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
   }
